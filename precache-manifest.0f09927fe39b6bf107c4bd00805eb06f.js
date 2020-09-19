74de41464218e7e30a90eca8b809ee2a4aefa737
Daniele de Moraes <danimoraes.barbosa@gmail.com>
2020-09-19 16:34:29 -0300

    Updates

---
+++ precache-manifest.0f09927fe39b6bf107c4bd00805eb06f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28eda3cea46e589879e07bcf1dce7a7f",
+    "url": "/playlists/index.html"
+  },
+  {
+    "revision": "128bae3a712ee5a04c71",
+    "url": "/playlists/static/css/main.56535463.chunk.css"
+  },
+  {
+    "revision": "6be307e88cf62481a9ba",
+    "url": "/playlists/static/js/2.0f6482e6.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/playlists/static/js/2.0f6482e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128bae3a712ee5a04c71",
+    "url": "/playlists/static/js/main.28e5627f.chunk.js"
+  },
+  {
+    "revision": "3bae2cfec6be03948e8b",
+    "url": "/playlists/static/js/runtime-main.437ef376.js"
+  },
+  {
+    "revision": "807fcd77857d8dfcf8692a056680ce7a",
+    "url": "/playlists/static/media/ifood-logo.807fcd77.PNG"
+  },
+  {
+    "revision": "c3122a410826b2fdfe105857c19eb92b",
+    "url": "/playlists/static/media/no-image.c3122a41.jpg"
+  },
+  {
+    "revision": "96a319e006c8f619e4e6f1c5e03e731f",
+    "url": "/playlists/static/media/spotify-logo.96a319e0.png"
+  }
+]);
